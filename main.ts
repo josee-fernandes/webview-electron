03deb946f8baddf3126f1d2e6055241911607b63
josee-fernandes <jose-vitor-7@hotmail.com>
2021-04-20 01:45:01 -0300

    Change to typescript

---
+++ main.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from 'electron'
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0) createWindow()
+})
